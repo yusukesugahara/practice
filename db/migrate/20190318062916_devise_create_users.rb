bae9005e1db28960e1dee92b2b57e8db64160e1c
yusukesugahara <yusukesugahara@sugawarayuusukenoMacBook-Pro.local>
2019-05-12 23:45:44 +0900

    Add_profile_icon

--- db/migrate/20190318062916_devise_create_users.rb
+++ db/migrate/20190318062916_devise_create_users.rb
@@ -5,6 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
+      t.string :image, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :nickname ,null: false, default: ""
       ## Recoverable
