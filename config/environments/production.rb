2d9e74b3c8048cfaef4b330ebcef14d1de71db0e
yusukesugahara <yusukesugahara@sugawarayuusukenoMacBook-Pro.local>
2019-05-13 15:24:55 +0900

    Setting-mail-1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  host = 'warm-falls-95935.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = host
+
+
 end
