c30267d784b121600b0ee292a90b4f3479f198dc
yusukesugahara <yusukesugahara@sugawarayuusukenoMacBook-Pro.local>
2019-05-13 13:53:49 +0900

    Setting-s3-2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region: 'ap-northeast-1'#リージョンをUS以外にしたかたはそのリージョンに変更
   }
 
