e2c8e722fbd84b591da8d0c267b1012f2b49a23c
yusukesugahara <yusukesugahara@sugawarayuusukenoMacBook-Pro.local>
2019-05-13 14:50:59 +0900

    Setting-s3-6

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production?
+  # if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'practice-11'
@@ -11,7 +11,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION'],
+      region: 'ap-northeast-1',
       path_style: true
     }
   else
