86e7ab03fcbe55837611a4e78eeae5914dd840a5
yusukesugahara <yusukesugahara@sugawarayuusukenoMacBook-Pro.local>
2019-05-13 14:52:43 +0900

    Setting-s3-7

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  # if Rails.env.production?
+  if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'practice-11'
