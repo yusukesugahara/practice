8febdf43e9a1d4c390549e6f968e112dbf6ec11d
yusukesugahara <yusukesugahara@sugawarayuusukenoMacBook-Pro.local>
2019-05-20 15:42:07 +0900

    Add-setting-sendgrid-2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,16 @@ Rails.application.configure do
   host = 'warm-falls-95935.herokuapp.com'
   Rails.application.routes.default_url_options[:host] = host
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 end
